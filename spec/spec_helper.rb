fd3db20c903acf05f26c2fdb83fc657b86228893
Mendy <mendybeltcohen@gmail.com>
2018-06-12 19:39:17 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,3 @@
 RSpec.configure do |config|
   config.order = :default
 end
-def turn_count(board)
-  count = 0
-  board.each do |entry|
-    if entry != " "
-      count += 1
-    end
-  end
-  puts count
-end
-
-def current_player(board)
-  if turn_count(board) % 2 == 0
-     return "O"  
-   else
-     return "X"
-   end
-end
